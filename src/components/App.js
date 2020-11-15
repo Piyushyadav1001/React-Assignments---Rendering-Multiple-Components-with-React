a1757cc8401c04fe852ac96418ffec2af0cd1715
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-15 21:12:53 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,13 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 data-ns-test="project-name">Covidline</h1>
+      <h3 data-ns-test="project-description">
+        An online platform for learning.
+      </h3>
+    </div>
+  );
 };
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-let e1 = document.getElementById("proj");
-let e2 = document.getElementById("prodes");
-e1.setAttribute("project-name", "covidline");
-e2.setAttribute(
-  "project-description",
-  "covidline is a online learming platform for school kids"
-);
-e1.innerHTML = e1.getAttribute("project-name");
-e2.innerHTML = e2.getAttribute("project-description");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
